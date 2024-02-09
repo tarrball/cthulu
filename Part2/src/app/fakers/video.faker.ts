59c4925d2c796ce4c88b1a4de433e75afbdc56a3
Andrew Tarr <atarr@dontpaniclabs.com>
2024-02-09 11:49:24 -0600

    part 3 solutionish

--- Part2/src/app/fakers/video.faker.ts
+++ Part2/src/app/fakers/video.faker.ts
@@ -6,6 +6,10 @@ export function generateMockVideo(): Video {
     id: faker.number.int({ min: 1, max: 100000 }),
     title: faker.lorem.words(3),
     description: faker.lorem.paragraph(1),
+    thumbnailBackground: faker.image.urlPicsumPhotos({
+      width: 375,
+      height: 300,
+    }),
     thumbnailFeatured: faker.image.urlPicsumPhotos({ width: 340, height: 120 }),
     thumbnailWide: faker.image.urlPicsumPhotos({ width: 125, height: 100 }),
     thumbnailTall: faker.image.urlPicsumPhotos({ width: 100, height: 125 }),
--- Part3/src/app/pages/more-details/more-details.page.ts
+++ Part3/src/app/pages/more-details/more-details.page.ts
@@ -5,22 +5,74 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
+  IonLabel,
+  IonButton,
+  IonIcon,
+  IonNote,
+  IonText,
+  IonBackButton,
+  IonButtons,
 } from '@ionic/angular/standalone';
 import { Video } from '../../data-contracts/video';
+import { addIcons } from 'ionicons';
+import {
+  addCircleOutline,
+  closeCircleOutline,
+  play,
+  shareOutline,
+  thumbsDownOutline,
+  thumbsUpOutline,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-more-details',
   templateUrl: 'more-details.page.html',
   styleUrls: ['more-details.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonModal, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonButtons,
+    IonBackButton,
+    IonText,
+    IonNote,
+    IonIcon,
+    IonButton,
+    IonLabel,
+    IonHeader,
+    IonModal,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+  ],
 })
 export class MoreDetailsPage {
   @Input() modal: IonModal = null!;
 
   @Input() video: Video = null!;
 
+  public interactions: Interaction[] = [
+    { name: 'My Stuff', icon: 'add-circle-outline' },
+    { name: 'Share', icon: 'share-outline' },
+    { name: 'Like', icon: 'thumbs-up-outline' },
+    { name: 'Dislike', icon: 'thumbs-down-outline' },
+  ];
+
+  constructor() {
+    addIcons({
+      addCircleOutline,
+      closeCircleOutline,
+      play,
+      shareOutline,
+      thumbsDownOutline,
+      thumbsUpOutline,
+    });
+  }
+
   public async closeModal() {
     await this.modal.dismiss();
   }
+}
+
+interface Interaction {
+  name: string;
+  icon: string;
 }
